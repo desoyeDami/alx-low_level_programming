53bf7435f1e38698155a7d4ad977835e77742eab
desoyeDami <desoyedami@gmail.com>
2023-08-10 22:06:01 +0000

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%lu", sizeof(char);
+	printf("%lu", sizeof(int);
+	printf("%lu", sizeof(float);
+	printf("%lu", sizeof(double);
+	return 0;
+}
