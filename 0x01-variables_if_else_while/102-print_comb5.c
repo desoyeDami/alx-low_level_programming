f7fd0e3d388ab6a56eb2ec4d431ccccdce85dffa
desoyeDami <desoyedami@gmail.com>
2023-08-11 21:44:06 -0700

    updat;

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,7 +5,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) {
+int main(void) 
+{
 	int num1, num2;
 	for (num1 = 0; num1 <= 99; num1++) 
 	{
@@ -15,7 +16,7 @@ int main(void) {
 			putchar('0' + num1 % 10);
 			putchar(' ');
 			putchar('0' + num2 / 10);
-			putchar('1' + num2 % 10);
+			putchar('!0' + num2 % 10);
 			if (num1 != 99 || num2 != 99) {
 			putchar(',');
 			putchar(' ');
