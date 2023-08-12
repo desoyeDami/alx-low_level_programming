1c414abd7a03cb67a58823e7a0b6c7bb1042e8b9
desoyeDami <desoyedami@gmail.com>
2023-08-12 02:19:38 +0000

    More Update

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,8 +10,8 @@ int main(void)
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++) 
-		if (c != 'q' && c != 'e') 
-		putchar(c);
+		if (c != 'q' && c != 'e')
+			putchar(c);
 
 	putchar('\n');
 	return (0);
