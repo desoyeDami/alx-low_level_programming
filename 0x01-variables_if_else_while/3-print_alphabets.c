28f65bd337358de59134d125b3797605e2e7f041
desoyeDami <desoyedami@gmail.com>
2023-08-11 18:34:42 -0700

    Variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char c;
+char b;
+
+for ( c = 'a';  c<= 'z'; c++) 
+putchar(c);
+for ( b='A'; b<= 'Z'; b++ )
+putchar(b);
+
+    	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int c;
+char b;
+
+for ( c = '0';  c<= '9'; c++) 
+putchar(c);
+for ( b='a'; b<= 'f'; b++ )
+putchar(b);
+
+    	return (0);
+}
