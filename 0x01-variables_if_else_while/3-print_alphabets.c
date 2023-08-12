b025643b8326219fb417ea0ab2c7849d2e910660
desoyeDami <desoyedami@gmail.com>
2023-08-12 02:08:00 +0000

    More Update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,13 +10,11 @@ int main(void)
 	char c;
 	char b;
 
-	for (c = 'a';  c <= 'z'; c++){
-		putchar(c);
-	}
-	for (b = 'A'; b <= 'Z'; b++){
-	putchar(b);
-	}
+	for (c = 'a';  c <= 'z'; c++)
+		putchar(c);}
+	for (b = 'A'; b <= 'Z'; b++)
+		putchar(b);
 
 	putchar('\n');
-    	return (0);
+	return (0);
 }
