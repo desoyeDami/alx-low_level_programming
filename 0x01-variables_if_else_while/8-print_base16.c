b2e96427ab7ab9712afe66fb1cee30e4c6a2ea99
desoyeDami <desoyedami@gmail.com>
2023-08-14 20:59:32 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,13 +7,14 @@
  */
 int main(void)
 {
-int c;
-char b;
+	int c;
+	char b;
 
-for (c = '0';  c <= '9'; c++) 
+	for (c = '0';  c <= '9'; c++)
 		putchar(c);
-	for (b ='a'; b <= 'f'; b++)
+	for (b = 'a'; b <= 'f'; b++)
 		putchar(b);
 
+	putchar('\n');
 	return (0);
 }
