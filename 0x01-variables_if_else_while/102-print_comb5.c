bd7bc9b12bc8df5872db87ac2e09a3f4f779ea2a
desoyeDami <desoyedami@gmail.com>
2023-08-14 22:14:07 +0000

    Retrieving last digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,24 +5,26 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	int num1, num2;
-	for (num1 = 0; num1 <= 99; num1++) 
+
+	for (num1 = 0; num1 <= 99; num1++)
 	{
 		for (num2 = num1; num2 <= 99; num2++)
- 		{
+		{
 			putchar('0' + num1 / 10);
 			putchar('0' + num1 % 10);
 			putchar(' ');
 			putchar('0' + num2 / 10);
 			putchar('1' + num2 / 10);
-			if (num1 != 99 || num2 != 99) {
+			if (num1 != 99 || num2 != 99)
+			{
 			putchar(',');
 			putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
-	return 0;
+	return (0);
 }
