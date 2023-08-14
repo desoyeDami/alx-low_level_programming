6ec7a1575690e1a54115b32829836aa0001313aa
desoyeDami <desoyedami@gmail.com>
2023-08-14 20:13:37 +0000

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-int i;
+	int i;
 
-for (i = '0';  i <= '9'; i++) 
-		putchar(i);	
-	putchar('/n');
+	for (i = '0';  i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
 	return (0);
 }
