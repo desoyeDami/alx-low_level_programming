38fe54ce57eb50e142e2add222464bb4eac303c4
desoyeDami <desoyedami@gmail.com>
2023-08-14 21:05:05 +0000

    Single Digit Numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,14 +7,18 @@
  */
 int main(void)
 {
-int c;
+	int c;
 
-for ( c = '0';  c<= '9'; c++) {
-putchar(c);
-if  ( c < '9' ){
-putchar(',');
-putchar(' ');
-}
-}
-    	return (0);
+	for (c = '0';  c <= '9'; c++)
+	{
+		putchar(c);
+		if  (c < '9')
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
 }
