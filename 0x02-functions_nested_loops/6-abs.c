a045af78d2352aa828a20ac1848166facea0a250
desoyeDami <desoyedami@gmail.com>
2023-08-16 00:58:55 +0000

    Updated Alpha Function

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,22 +1,19 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - function that computes the absolute value of an integer
- *
- * Return: Always 0.
+ * _abs - computes the absolute value of an integer.
+ * @i: the number to be computed.
+ * Return: Absolute value of number or zero
  */
-int main(void)
-{
-    int r;
 
-    r = _abs(-1);
-    printf("%d\n", r);
-    r = _abs(0);
-    printf("%d\n", r);
-    r = _abs(1);
-    printf("%d\n", r);
-    r = _abs(-98);
-    printf("%d\n", r);
-    return (0);
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	int abs_sub;
+
+	abs_val = i  * -1;
+	return (abs_sub);
+	}
+	return (c);
 }
