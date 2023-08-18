5201b7eae26b80b485419e75b0c16f8985c635bb
desoyeDami <desoyedami@gmail.com>
2023-08-18 21:42:41 +0000

    debbuged files

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include <studio>
+
+void positive_or_negative(int i)
+{
+	If (i < 0) 
+		printf("%d is negative" i);
+	else if (i > 0)
+		printf("%d is positive" i);
+	else
+		printf("%d is zero" i);
+}
+
