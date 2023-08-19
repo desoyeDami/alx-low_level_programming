7d6657ed99b5d0c26d8e6a2ddaa3cca5071b40a6
desoyeDami <desoyedami@gmail.com>
2023-08-19 22:17:11 +0000

    Gosh! How do I multiply?

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - function multiplies two integers
+ * @a: first integer to be multiplied
+ * @b: second integer to be multiplied
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
