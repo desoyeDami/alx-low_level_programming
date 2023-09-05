1788d06e98911c559220df1defcfd920de936eef
desoyeDami <desoyedami@gmail.com>
2023-09-05 21:16:22 +0000

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,7 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
-	char *ptr = malloc(size * sizeof(c));
+	char *ptr = malloc(size * sizeof(char));
 
 	if (ptr == 0)
 		return (NULL);
