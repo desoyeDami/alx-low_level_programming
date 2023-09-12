a447b875906ed4597b988b5dce82275737004a8f
desoyeDami <desoyedami@gmail.com>
2023-09-12 01:46:46 +0000

    Disposed some unused files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,8 @@ struct dog
 	float age;
 	char *owner;
 };
-
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
@@ -25,4 +25,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 
 void free_dog(dog_t *d);
 
-#endif
+#endif /*__DOG_H__*/
