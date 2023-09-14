058b5c46db18f8ec5722ad27e7a4844f3d6197d2
desoyeDami <desoyedami@gmail.com>
2023-09-14 21:41:30 +0000

    Task 4

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,28 +2,6 @@
 #include <stdlib.h>
 
 /**
- * print_opcodes - Print the opcodes of the main function.
- * @num_bytes: The number of bytes to print.
- */
-void print_opcodes(int num_bytes)
-{
-	int i;
-	unsigned char *main_addr = (unsigned char *)print_opcodes;
-
-	if (num_bytes < 0)
-	{
-		printf("Error\n");
-		exit(2);
-	}
-
-	for (i = 0; i < num_bytes; i++)
-	{
-		printf("%02x ", main_addr[i]);
-	}
-	printf("\n");
-}
-
-/**
  * main - Entry point of the program
  * @argc: The number of command-line arguments
  * @argv: An array of command-line argument strings
@@ -33,16 +11,32 @@ void print_opcodes(int num_bytes)
  */
 int main(int argc, char *argv[])
 {
+	int i;
+
 	if (argc != 2)
 	{
-		printf("Usage: %s number_of_bytes\n", argv[0]);
+		printf("Error\n");
 		return (1);
 	}
 
 	int num_bytes = atoi(argv[1]);
 
-	print_opcodes(num_bytes);
+	if (num_bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
 
+	unsigned char *opcode = (unsigned char *)main;
+
+	for (i = 0; i < num_bytes; i++)
+	{
+		printf("%02x", opcode[i]);
+		if (i < num_bytes - 1)
+			printf(" ");
+	}
+
+	printf("\n");
 	return (0);
 }
 
