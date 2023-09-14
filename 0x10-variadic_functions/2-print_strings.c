c15f0b75b050739d304509ced72324ede9262329
desoyeDami <desoyedami@gmail.com>
2023-09-14 23:37:01 +0000

    Task 2 bug fixed

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,7 +1,6 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
-
 /**
  * print_strings - Prints strings followed by a separator.
  * @separator: The string to be printed between strings.
@@ -13,14 +12,14 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	char *str
+	char *str;
 
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
 
-		str = va_arg(args, int);
+		str = va_arg(args, char *);
 
 		if (str)
 			printf("%s", str);
