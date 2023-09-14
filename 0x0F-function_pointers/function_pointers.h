057859e228ef2ab57727a3940a68ed23651a95ad
desoyeDami <desoyedami@gmail.com>
2023-09-14 20:49:51 +0000

    Function pointers Task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,3 @@
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-
-
-#endif
