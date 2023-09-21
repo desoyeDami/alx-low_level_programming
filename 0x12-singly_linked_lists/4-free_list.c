7c72924d65c2e10bd1ecae35b4af8ae02ceae69f
desoyeDami <desoyedami@gmail.com>
2023-09-21 21:57:09 +0000

    task4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list_t list.
+ * @head: Pointer to the head of the list.
+ */
+void free_list(list_t *head)
+{
+	if (head != NULL)
+		free(head);
+
+}
