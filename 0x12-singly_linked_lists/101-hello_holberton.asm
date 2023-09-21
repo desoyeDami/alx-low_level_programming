ec1123c50f95c0a5391fdc6522248fa8695e76d9
desoyeDami <desoyedami@gmail.com>
2023-09-21 23:36:17 +0000

    Task 6/101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,1 +1,17 @@
+  global main
+  extern printf
 
+  section .text   
+  main:
+      push rbp
+      mov rbp, rsp
+
+      ; Call printf
+      mov rdi, msg
+      call printf
+
+      pop rbp
+      ret
+
+  section .data
+  msg: db 'Hello, Holberton', 10, 0
