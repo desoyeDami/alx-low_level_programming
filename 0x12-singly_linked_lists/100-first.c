0120d978d6003b5764882fc8075c29526afc9ecf
desoyeDami <desoyedami@gmail.com>
2023-09-21 22:57:28 +0000

    Task 5/100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * string - Function to execute before main.
+ */
+void string(void)
+{
+printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+}
