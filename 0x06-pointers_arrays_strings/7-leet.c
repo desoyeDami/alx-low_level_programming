4d3afadcb4b6c0f10d72196a115d08f0542fa295
desoyeDami <desoyedami@gmail.com>
2023-09-22 20:26:16 +0000

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,22 +10,17 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char *c = "aAeEoOtTlL";
+	char *d = "4433007711";
 
-	for (i = 0; str[i] != '\0'; i++)
+	for (i = 0; *(str + i); i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
-			str[i] = '4';
-		else if (str[i] == 'e' || str[i] == 'E')
-			str[i] = '3';
-		else if (str[i] == 'o' || str[i] == 'O')
-			str[i] = '0';
-		else if (str[i] == 't' || str[i] == 'T')
-			str[i] = '7';
-		else if (str[i] == 'l' || str[i] == 'L')
-			str[i] = '1';
-		else
-			str[i] = str[i];
+		for (j = 0; j <= 9; j++)
+		{
+			if(c[j] == str[i])
+				str[i] = d[j];
+		}
 	}
 
 	return (str);
