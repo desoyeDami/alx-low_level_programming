6eb81b428d7eee51e9bb196abb4b20d04e0457e1
desoyeDami <desoyedami@gmail.com>
2023-09-27 01:37:09 +0000

    Summing all items n item in a list

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Computes the sum of all elements in a linked list.
+ * @head: A pointer to the head of the linked list.
+ *
+ * Return: The sum of all elements in the list. If the list is empty, returns 0.
+ */
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
