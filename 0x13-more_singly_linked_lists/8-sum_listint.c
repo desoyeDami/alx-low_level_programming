9f2b031eac6589f3666d2c9be9b9b74fdd5a0ab0
desoyeDami <desoyedami@gmail.com>
2023-09-27 01:38:43 +0000

    Adhered to betty

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -4,7 +4,7 @@
  * sum_listint - Computes the sum of all elements in a linked list.
  * @head: A pointer to the head of the linked list.
  *
- * Return: The sum of all elements in the list. If the list is empty, returns 0.
+ * Return: The sum of all elements in the list. If the list is empty, rtns 0.
  */
 int sum_listint(listint_t *head)
 {
