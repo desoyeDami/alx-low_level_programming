8d51f391dda7b793c75a7f7a59168c99a7c0a881
desoyeDami <desoyedami@gmail.com>
2023-10-02 16:09:45 +0100

    Read File

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,8 +13,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t bytes_rd;
 	char buf[1024];
 
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
 	fd = open(filename, O_RDONLY);
-	if (fd == -1 || filename == NULL)
+	if (fd == -1)
 	{
 		return (0);
 	}
