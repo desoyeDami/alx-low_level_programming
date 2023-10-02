7873e1adfe2ff6e19ab0bba1b20392af954dcdab
desoyeDami <desoyedami@gmail.com>
2023-10-02 18:19:56 +0100

    Reading a text file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,31 +10,42 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_rd;
-	char buf[1024];
+	ssize_t bytes_rd, written_bytes;
+	char *buf;
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
+	/* Open a File.*/
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-
+	/* Allocate memory to the buffer where files would be stored.*/
+	buf = malloc(sizeof(char) * letters);
+	if (buf == 0)
+		return (0);
+	/* Read file characters and save them to the buffer.*/
 	bytes_rd = read(fd, buf, letters);
-
 	if (bytes_rd < 0)
+		return (0);
+	/*If EOF has been reached, add a new line.*/
+	if ((size_t)bytes_rd < letters)
 	{
+		buf[bytes_rd] = '\n';
+		bytes_rd++;
+	}
+	/* Write stored characters to STDOUT*/
+	written_bytes = write(STDOUT_FILENO, buf, bytes_rd);
+	if (written_bytes != bytes_rd)
+	{
+		free(buf);
+		close(fd);
 		return (0);
 	}
-	write(STDOUT_FILENO, buf, bytes_rd);
-
-	write(STDOUT_FILENO, "\n", 1);
-	bytes_rd++;
-
 	close(fd);
+	free(buf);
 	return (bytes_rd);
 }
