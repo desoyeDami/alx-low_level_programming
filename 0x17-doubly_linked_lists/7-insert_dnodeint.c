923739724a296540741d808ffc841df4bd7de4c6
desoyeDami <desoyedami@gmail.com>
2023-11-09 23:43:17 +0100

    Fix bug

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,17 +10,20 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *temp;
+	dlistint_t *new_node, *tmp;
 	unsigned int count = 0;
 
 	if (h == NULL)
 		return (NULL);
+
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
 	new_node->next = NULL;
 	new_node->prev = NULL;
+
 	if (idx == 0)
 	{
 		new_node->next = *h;
@@ -29,23 +32,26 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new_node;
 		return (new_node);
 	}
-	temp = *h;
-	while (temp != NULL)
+	tmp = *h;
+	while (count < idx - 1)
 	{
-		if (count == idx)
-			break;
+		if (tmp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		tmp = tmp->next;
 		count++;
-		temp = temp->next;
 	}
-	if (count < idx || temp == NULL)
+	if (tmp == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
-	new_node->next = temp;
-	new_node->prev = temp->prev;
-	if (temp->prev != NULL)
-		temp->prev->next = new_node;
-	temp->prev = new_node;
+	new_node->next = tmp->next;
+	new_node->prev = tmp;
+	if (tmp->next != NULL)
+		tmp->next->prev = new_node;
+	tmp->next = new_node;
 	return (new_node);
 }
